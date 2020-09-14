c4f8aa3f0f16bf47baac6895a756224713715d05
Ray1129 <ab89141@gmail.com>
2020-09-15 01:15:56 +0800

    first commit

---
+++ data_preprocess.py
@@ -0,0 +1,18 @@
+"""
+Creat by SH Huang.
+"""
+import librosa
+
+
+
+class fbank:
+    def __init__(self, x=None, sr=16000, melfilter=128, banknum=79):
+        
+        self.x = x
+
+
+
+
+a = fbank()
+
+print(a)
