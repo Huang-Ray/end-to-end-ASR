ae1cd0302979ac6b1460e26b0ed82d64c17e8193
Ray1129 <ab89141@gmail.com>
2020-09-15 01:24:46 +0800

    first commit

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,16 +3,20 @@ Creat by SH Huang.
 """
 import librosa
 
-
+from keras.layers import Dense
 
 class fbank:
-    def __init__(self, x=None, sr=16000, melfilter=128, banknum=79):
+    def __init__(self, x=None, sr=22500, melfilter=128, banknum=79):
         
         self.x = x
+        self.sr = sr
+        self.melfilter = melfilter
+        self.banknum = banknum
+    
+    def go(self):
+
+        print("go")
 
 
 
 
-a = fbank()
-
-print(a)
